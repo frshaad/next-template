f03d524acd525754560088a07b3563bbb1c79bba
Farshad Hatami <farshad.hatami95@gmail.com>
2024-03-20 14:38:45 +0330

    default

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,15 @@ export default function HomePage() {
 
   return (
     <div>
-      <h1 className='text-xl font-semibold'>Hello World</h1>
+      <h1 className="text-xl font-semibold">Hello World</h1>
       <Link href={'/'}>Home</Link>
-      <input type='text' />
+      <input type="text" />
       <button
         className={cn('py-3 border px-2', {
           'py-2 px-4 rounded-lg': isLarge,
         })}
       >
-        <p className='flex text-black'></p>
+        <p className="flex text-black"></p>
       </button>
     </div>
   );
