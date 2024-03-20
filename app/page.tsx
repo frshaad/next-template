19bd1c3bcd8f36d7257e3c626942a22fb9ad1b82
Farshad Hatami <farshad.hatami95@gmail.com>
2024-03-20 14:38:01 +0330

    default

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { lazy, useState } from 'react';
+
+import { inter } from '@/lib/font';
+import { cn } from '@/lib/utils';
+
 export default function HomePage() {
-  return <h1 className='text-xl font-semibold'>Hello World</h1>;
+  const [isLarge, setIsLarge] = useState(false);
+
+  return (
+    <div>
+      <h1 className='text-xl font-semibold'>Hello World</h1>
+      <Link href={'/'}>Home</Link>
+      <input type='text' />
+      <button
+        className={cn('py-3 border px-2', {
+          'py-2 px-4 rounded-lg': isLarge,
+        })}
+      >
+        <p className='flex text-black'></p>
+      </button>
+    </div>
+  );
 }
