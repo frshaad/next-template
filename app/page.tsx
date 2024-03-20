3c9921f38539456be8455e27b93ed2f5b3ea1287
Farshad Hatami <farshad.hatami95@gmail.com>
2024-03-20 18:18:48 +0330

    add ts eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { lazy, useState } from 'react';
-
-import { inter } from '@/lib/font';
-import { cn } from '@/lib/utils';
-
 export default function HomePage() {
-  const [isLarge, setIsLarge] = useState(false);
-
-  return (
-    <div>
-      <h1 className="text-xl font-semibold">Hello World</h1>
-      <Link href={'/'}>Home</Link>
-      <input type="text" />
-      <button
-        className={cn('py-3 border px-2', {
-          'py-2 px-4 rounded-lg': isLarge,
-        })}
-      >
-        <p className="flex text-black"></p>
-      </button>
-    </div>
-  );
+  return <h1 className="text-xl font-semibold">Hello World</h1>;
 }
