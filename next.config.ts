a208011e579178efad0bea92bb52baefc5177394
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-12-07 18:20:07 +0330

    unflag some experimental next onfigs

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typedRoutes: true,
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
   experimental: {
     viewTransition: true,
-    webVitalsAttribution: ['CLS', 'LCP', 'INP'],
     turbopackFileSystemCacheForDev: true,
     typedEnv: true,
   },
